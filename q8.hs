a408bf8e5b92a60a831519f81740032253e88545
afmireski <afmireski@outlook.com>
2023-11-11 20:42:21 -0300

    Update q8.hs

--- q8.hs
+++ q8.hs
@@ -3,7 +3,7 @@ listInterception [] [] [] = [] -- Nada informado
 listInterception [] [] tmp = [] -- Ambas as listas foram processadas
 listInterception [] l2 [] = [] 
 listInterception [] l2 tmp = [] 
-listInterception l1 [] [] = [] -- L2
+listInterception l1 [] [] = [] 
 listInterception l1 [] tmp = listInterception (tail l1) tmp []
 listInterception l1 l2 tmp
  | head l1 /= head l2 && null tmp = listInterception l1 (tail l2) l2
